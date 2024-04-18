93cfbaf39aae76bb2a12b08933f8915fc661e15f
Md Mohsin <siam.mohsin2005@gmail.com>
2024-04-18 21:38:25 +0600

    Add files via upload

---
+++ week 4/Day 3/N_Dull_Operation.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pi pair<int, int>
+#define vi vector<int>
+#define ln '\n'
+using namespace std;
+const ll N = 2e5 + 9;
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n << " " << n - 1 << ln;
+    }
+    return 0;
+}
