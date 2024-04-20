3f6da2f10af518835eaaba4afde80076683c6004
Md Mohsin <siam.mohsin2005@gmail.com>
2024-04-20 19:55:53 +0600

    Add files via upload

---
+++ week 5/Day 2/A_Binary_Search.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pi pair<int, int>
+#define vi vector<int>
+#define ln '\n'
+using namespace std;
+const ll N = 2e5 + 9;
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, q;
+    cin >> n >> q;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    while (q--)
+    {
+        ll x;
+        cin >> x;
+        ll l = 0, r = n - 1, idx = -1, mid;
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (a[mid] == x)
+            {
+                idx = mid;
+                break;
+            }
+            else if (a[mid] > x)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        if (idx != -1)
+        {
+            yes;
+        }
+        else
+        {
+            no;
+        }
+    }
+    return 0;
+}
---
+++ week 5/Day 2/C_Closest_to_the_Right.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pi pair<int, int>
+#define vi vector<ll>
+#define ln '\n'
+using namespace std;
+const ll N = 2e5 + 9;
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, q;
+    cin >> n >> q;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    while (q--)
+    {
+        ll x;
+        cin >> x;
+        ll l = 0, r = a.size() - 1, idx = n, mid;
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (a[mid] >= x)
+            {
+                idx = mid;
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        cout << idx + 1 << ln;
+    }
+    return 0;
+}
