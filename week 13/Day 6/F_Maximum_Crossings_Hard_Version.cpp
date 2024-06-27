bdb84067f742e34e9a44671e890cb7c26edceb54
Md Mohsin <siam.mohsin2005@gmail.com>
2024-06-27 21:43:09 +0600

    Add files via upload

---
+++ week 13/Day 6/F_Maximum_Crossings_Hard_Version.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define fastio()                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ln '\n'
+#define ll long long
+typedef vector<ll> vi;
+typedef pair<ll, ll> pi;
+const ll N = 1e5 + 9;
+const ll mod = 1e9 + 7;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // less -> set, less_equal -> multi-set
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    pbds<pi> pb;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans += i - pb.order_of_key({a[i], 0});
+        pb.insert({a[i], i});
+    }
+    cout << ans << ln;
+}
+
+int main(int argc, char const *argv[])
+{
+    fastio();
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
